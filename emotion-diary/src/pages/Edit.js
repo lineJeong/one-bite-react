90eea03d411cd87ffb8a73ae47fd2a39da0aa5f2
lineJeong <line.jeong32@gmail.com>
2022-10-22 23:53:52 +0900

    페이지 라우팅 2 - React Router 응용

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,7 +1,29 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 function Edit() {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+  console.log(id);
+  console.log(mode);
+
   return (
     <div>
       <h1>Edit</h1>
+      <button
+        onClick={() =>
+          setSearchParams({
+            id: 10,
+            mode: "winterlood",
+          })
+        }
+      >
+        QS 바꾸기
+      </button>
+      <button onClick={() => navigate("/")}>HOME으로 가기</button>
+      <button onClick={() => navigate(-1)}>뒤로 가기</button>
     </div>
   );
 }
