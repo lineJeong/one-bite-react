65c424341888fd284891a75e6a08cda915509a30
lineJeong <line.jeong32@gmail.com>
2022-10-24 23:57:56 +0900

    페이지 구현 - 홈(/)

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -36,8 +36,17 @@ const reducer = (state, action) => {
 export const DiaryStaticContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  { id: 1, emotion: 1, content: "오늘의 일기 1번", date: 1666617633201 },
+  { id: 2, emotion: 2, content: "오늘의 일기 2번", date: 1666617633202 },
+  { id: 3, emotion: 3, content: "오늘의 일기 3번", date: 1666617633203 },
+  { id: 4, emotion: 4, content: "오늘의 일기 4번", date: 1666617633204 },
+  { id: 5, emotion: 5, content: "오늘의 일기 5번", date: 1666617633205 },
+  { id: 6, emotion: 5, content: "오늘의 일기 12월", date: 1669857010010 },
+];
+
 function App() {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
 
   const dataId = useRef(0);
 
